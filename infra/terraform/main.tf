8f952e8f1f220893bf89676620a20ce1f40e42d6
Suriyaprasad U K <83288690+suriyaspd74@users.noreply.github.com>
2025-05-29 18:18:37 +0530

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -2,9 +2,8 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "ecs_cluster" {
-  source = "terraform-aws-modules/ecs/aws"
-  name   = var.cluster_name
+resource "aws_ecs_cluster" "this" {
+  name               = var.cluster_name
   capacity_providers = ["FARGATE"]
 }
 
@@ -16,7 +15,8 @@ resource "aws_ecs_task_definition" "app" {
   memory                   = "512"
   execution_role_arn       = var.execution_role_arn
   task_role_arn            = var.task_role_arn
-  container_definitions    = jsonencode([
+
+  container_definitions = jsonencode([
     {
       name      = "app"
       image     = var.image_url
@@ -33,7 +33,7 @@ resource "aws_ecs_task_definition" "app" {
 
 resource "aws_ecs_service" "app" {
   name            = "my-app-service"
-  cluster         = module.ecs_cluster.cluster_id
+  cluster         = aws_ecs_cluster.this.id
   task_definition = aws_ecs_task_definition.app.arn
   desired_count   = 1
   launch_type     = "FARGATE"
@@ -43,4 +43,6 @@ resource "aws_ecs_service" "app" {
     security_groups  = [var.security_group_id]
     assign_public_ip = true
   }
+
+  depends_on = [aws_ecs_task_definition.app]
 }
